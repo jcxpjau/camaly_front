4240b5acfabe72b36681aac668585b36145446db
jcxpjau <jcxpjau@live.com>
2025-05-20 16:57:22 -0300

    mudanças

---
+++ app/routes/admin.dashboard.tsx
@@ -0,0 +1,12 @@
+import type { Route } from "../+types/root";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Dashboard() {
+  return <h1>Dashboard</h1>;
+}
---
+++ app/routes/admin.pay.tsx
@@ -0,0 +1,12 @@
+import type { Route } from "../+types/root";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Pay() {
+  return <h1>Payment</h1>;
+}
