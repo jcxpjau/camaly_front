d4465bc405b86878095e3e7dec37d49d9ed2877a
jcxpjau <jcxpjau@live.com>
2025-05-22 17:33:30 -0300

    Ajustes

--- app/hooks/useCustomNavigate.ts
+++ app/hooks/useCustomNavigate.ts
@@ -1,10 +1,15 @@
 import { useNavigate } from "react-router";
+import { useSideBar } from "~/context/theme/sidebar.hooks";
 
 export function useCustomNavigate() {
   const navigate = useNavigate();
+  const {isOpen, toggleSidebar} = useSideBar();
 
   return (e: React.MouseEvent, route: string) => {
     e.preventDefault();
+    if( isOpen ) {
+      toggleSidebar();
+    }
     navigate(route);
   };
 }
